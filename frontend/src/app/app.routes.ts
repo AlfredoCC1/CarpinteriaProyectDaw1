c05b35492d757bc68aea4b383451a7641b2ec15b
Alfredo Castro <106498188+AlfredoCC1@users.noreply.github.com>
2025-12-18 14:13:40 -0500

    Intalacion de Librerias
    npm install @fortawesome/fontawesome-free

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { IndexComponent } from './pages/index/index';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {path: '', component: IndexComponent}
+];
